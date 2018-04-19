1141099e5105212082d8c12e4eb991ed0aa59bd3
Jeremy Brown <jeremy@zadarastorage.com>
2018-04-19 11:32:09 -0700

    Add project metadata to setuptools and define package requirements in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='zadarapy',
     version='0.1',
     packages=find_packages(),
+    install_requires=['configparser>=3.5.0', 'future>=0.15.2',
+                      'ipaddress>=1.0.17', 'terminaltables>=2.1.0'],
     url='https://github.com/zadarastorage/zadarapy',
     license='Apache License 2.0',
     author='Jeremy Brown',
