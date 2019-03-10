162b169a318792e9277fd5b751f12f0aa8ac3d5c
bshakur <bhaa@zadara.com>
2019-03-10 18:00:50 +0200

    ZAT: Fix delete_tenant. Add get_cloud
    Signed-off-by: bshakur <bhaa@zadara.com>

--- zadarapy/command_center/cloud.py
+++ zadarapy/command_center/cloud.py
@@ -12,6 +12,31 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
 # License for the specific language governing permissions and limitations
 # under the License.
+from zadarapy.validators import verify_vpsa_id, verify_boolean
+
+
+def get_cloud(session, cloud_name, return_type=None):
+    """
+    Retrieves details for current cloud
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = '/api/clouds/{0}.json'.format(cloud_name)
+    return session.get_api(path=path, return_type=return_type)
 
 
 def get_all_clouds(session, return_type=None):
@@ -30,7 +55,437 @@ def get_all_clouds(session, return_type=None):
     :returns: A dictionary or JSON data set as a string depending on
         return_type parameter.
     """
-    method = 'GET'
     path = '/api/clouds.json'
+    return session.get_api(path=path, return_type=return_type)
 
-    return session.call_api(method=method, path=path, return_type=return_type)
+
+def get_vpsa_from_cloud(session, cloud_name, vpsa_id, return_type=None):
+    """
+    Returns VPSA info from Command Center
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type vpsa_id: str
+    :param vpsa_id: VPSA ID. i.e: 126
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    verify_vpsa_id(vpsa_id)
+
+    path = "/api/clouds/{0}/vpsas/{1}.json".format(cloud_name, vpsa_id)
+    return session.get_api(path=path, return_type=return_type)
+
+
+def get_all_drives(session, cloud_name, return_type=None):
+    """
+    Get all command Center Drives
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/drives.json".format(cloud_name)
+    return session.get_api(path=path, return_type=return_type)
+
+
+def get_vlans(session, cloud_name, return_type=None):
+    """
+    Get all command Center VLANs
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/vlans.json".format(cloud_name)
+
+    return session.get_api(path=path, return_type=return_type)
+
+
+def delete_vlans(session, cloud_name, vlan_id, return_type=None):
+    """
+    Delete VLan
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type vlan_id: str
+    :param vlan_id: VLAN ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/vlans/{1}.json".format(cloud_name, vlan_id)
+    return session.delete_api(path=path, return_type=return_type)
+
+
+def get_tenants(session, cloud_name, return_type=None):
+    """
+    Get all command Center Tennants
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/cloud_users.json".format(cloud_name)
+    return session.get_api(path=path, return_type=return_type)
+
+
+def get_tenant(session, cloud_name, cloud_user, return_type=None):
+    """
+    Get all command Center Tennants
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type cloud_user: str
+    :param cloud_user: Cloud User
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/cloud_users/{1}.json".format(cloud_name, cloud_user)
+    return session.get_api(path=path, return_type=return_type)
+
+
+def get_storage_node(session, cloud_name, sn_id, return_type=None):
+    """
+    Get all command Center Tennants
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type sn_id: str
+    :param sn_id: Storage Node ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/nodes/{1}.json".format(cloud_name, sn_id)
+    return session.get_api(path=path, return_type=return_type)
+
+
+def get_storage_node_drives(session, cloud_name, sn_id, return_type=None):
+    """
+    Get all Storage Node drives
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type sn_id: str
+    :param sn_id: Storage Node ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/nodes/{1}/drives.json".format(cloud_name, sn_id)
+    return session.get_api(path=path, return_type=return_type)
+
+
+def reboot_storage_node(session, cloud_name, sn_id, return_type=None):
+    """
+    Reboot Storage Node
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type sn_id: str
+    :param sn_id: Storage Node ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/nodes/{1}/reboot.json".format(cloud_name, sn_id)
+    return session.post_api(path=path, return_type=return_type)
+
+
+def shutdown_storage_node(session, cloud_name, sn_id, return_type=None):
+    """
+    Shutdown Storage Node
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type sn_id: str
+    :param sn_id: Storage Node ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/nodes/{1}/shutdown.json".format(cloud_name, sn_id)
+    return session.post_api(path=path, return_type=return_type)
+
+
+def zsnap_storage_node(session, cloud_name, sn_id, return_type=None):
+    """
+    Create Storage Node ZSnap
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type sn_id: str
+    :param sn_id: Storage Node ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/nodes/{1}/zsnap.json".format(cloud_name, sn_id)
+    return session.post_api(path=path, return_type=return_type)
+
+
+def license_storage_node(session, cloud_name, sn_id, return_type=None):
+    """
+    Storage Node license
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type sn_id: str
+    :param sn_id: Storage Node ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/nodes/{1}/license.json".format(cloud_name, sn_id)
+    return session.post_api(path=path, return_type=return_type)
+
+
+def set_default_image(session, cloud_name, image_id, return_type=None):
+    """
+    Set default Image
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type image_id: str
+    :param image_id: Image ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/images/{1}/set_default.json".format(cloud_name, image_id)
+    return session.post_api(path=path, return_type=return_type)
+
+
+def delete_tenant(session, cloud_name, cloud_user, return_type=None):
+    """
+    Delete tenant
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type cloud_user: str
+    :param cloud_user: Cloud User ID
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    path = "/api/clouds/{0}/cloud_users/{1}/deallocate_vlan_id.json".format(cloud_name, cloud_user)
+    return session.delete_api(path=path, return_type=return_type)
+
+
+def allocate_vlan(session, cloud_name, cloud_user, vlan_id, force="NO", return_type=None):
+    """
+    Allocate VLAN from cloud user
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type cloud_user: str
+    :param cloud_user: Cloud User ID
+
+    :type vlan_id: str
+    :param vlan_id: VLAN ID
+
+    :type force: str
+    :param force: "YES", "NO"
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    force = verify_boolean(force, "force")
+
+    path = "/api/clouds/{0}/cloud_users/{1}/allocate_vlan_id.json".format(cloud_name, cloud_user)
+
+    body_values = {"vlan_id": vlan_id, "force": force}
+
+    return session.post_api(path=path, body=body_values, return_type=return_type)
+
+
+def deallocate_vlan(session, cloud_name, cloud_user, vlan_id, force="NO", return_type=None):
+    """
+    Allocate VLAN from cloud user
+
+    :type session: zadarapy.session.Session
+    :param session: A valid zadarapy.session.Session object.  Required.
+
+    :type cloud_name: str
+    :param cloud_name: Cloud Name: i.e: zadaraqa9
+
+    :type cloud_user: str
+    :param cloud_user: Cloud User ID
+
+    :type vlan_id: str
+    :param vlan_id: VLAN ID
+
+    :type force: str
+    :param force: "YES", "NO"
+
+    :type return_type: str
+    :param return_type: If this is set to the string 'json', this function
+        will return a JSON string.  Otherwise, it will return a Python
+        dictionary.  Optional (will return a Python dictionary by default).
+
+    :rtype: dict, str
+    :returns: A dictionary or JSON data set as a string depending on
+        return_type parameter.
+    """
+    force = verify_boolean(force, "force")
+
+    path = "/api/clouds/{0}/cloud_users/{1}/deallocate_vlan_id.json".format(cloud_name, cloud_user)
+
+    body_values = {"vlan_id": vlan_id, "force": force}
+
+    return session.post_api(path=path, body=body_values, return_type=return_type)
